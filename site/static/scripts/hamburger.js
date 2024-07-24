47b5505f05561ddf9e962b04a2f610776364bc8a
bwaklog <aditya.mh@outlook.com>
2024-07-24 18:44:19 +0530

    fix: card styling, (wip) buggy mobile nav

--- site/static/scripts/hamburger.js
+++ site/static/scripts/hamburger.js
@@ -4,23 +4,17 @@ menu.addEventListener("click", function () {
         if (menu.className === "closed") {
             menu.className = "open";
 
-            window.onscroll = function () {
-                window.scrollTo(0, 0);
-            };
-
             menu.innerHTML =
                 '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>';
 
             var links = document.getElementById("links");
-            var container = document.getElementById("container")
 
             links.style.zIndex = "100";
             links.style.opacity = "1";
             links.style.display = "flex";
+            links.style.backdropFilter = "blur(15px) brightness(0.5)"
+            links.style.webkitBackdropFilter = "blur(15px)"
         } else {
-            window.onscroll = function () {
-                window.scrollTo(window.scrollX, window.scrollY);
-            };
             menu.className = "closed";
             menu.innerHTML =
                 '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>';
@@ -30,6 +24,8 @@ menu.addEventListener("click", function () {
             links.style.zIndex = "-1";
             links.style.opacity = "0";
             links.style.display = "flex";
+            links.style.backdropFilter = "none"
+            links.style.webkitBackdropFilter = "none"
         }
     }
 });
@@ -38,16 +34,17 @@ window.addEventListener("resize", function () {
     var links = document.getElementById("links");
     var menu_icon = document.getElementById("menu_icon");
     if (window.innerWidth > 900) {
-        window.onscroll = function () {
-            window.scrollTo(window.scrollX, window.scrollY);
-        };
         links.style.opacity = "1";
+        links.style.backdropFilter = "blur(15px)"
+        links.style.webkitBackdropFilter = "blur(15px)"
         menu.className = "closed";
         menu.innerHTML =
             '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>';
     } else {
         if (menu.className === "closed") {
             links.style.opacity = "0";
+            links.style.backdropFilter = "blur(15px)"
+            links.style.webkitBackdropFilter = "blur(15px)"
         }
     }
 });
